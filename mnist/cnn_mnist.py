c133aabf4c54452c2512b7fd3691c4a953a34db4
李振洋 <lizhenyang@raintai.com>
2016-07-31 23:34:05 +0800

    mnist of cnn is ok

--- mnist/cnn_mnist.py
+++ mnist/cnn_mnist.py
@@ -7,6 +7,17 @@
 import tensorflow as tf
 
 
+# input and label
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+# session
+sess = tf.InteractiveSession()
+
+
 # weight
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
@@ -39,7 +50,7 @@ x_image = tf.reshape(x, [-1, 28, 28, 1])
 
 
 ## convolution and pooling
-h_conv1 = tf.nn.relu(conv2d(x_imge, W_conv1) + b_conv1)
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 
@@ -50,3 +61,49 @@ b_conv2 = bias_variable([64])
 ## convolution and pooling
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
+
+
+# full connect
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+
+# dropout layer
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+
+# readout layer
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+
+# Error: put initialize variables here will error.
+
+# train and test
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices = [1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# initialize variables
+sess.run(tf.initialize_all_variables())
+
+
+## train
+for i in range(1000):
+    batch = mnist.train.next_batch(50)
+    if i % 100 == 0:
+        train_accuracy = accuracy.eval(feed_dict = {x: batch[0], y_: batch[1], keep_prob: 1.0})
+        print 'training step %d, the accuracy: %g'%(i, train_accuracy)
+    train_step.run(feed_dict={x:batch[0], y_:batch[1], keep_prob:0.5})
+
+# test
+test_accuracy = accuracy.eval(feed_dict = {x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+print 'the test accuracy: %g'%test_accuracy
