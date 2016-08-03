e1484ce5229c1ca327286ab16d0c22e82a953849
李振洋 <lizhenyang@raintai.com>
2016-08-03 14:27:44 +0800

    training seems ok

--- street-number/cnn_st.py
+++ street-number/cnn_st.py
@@ -6,6 +6,7 @@
 
 import tensorflow as tf
 
+from softmax_st import *
 
 # variables that can be changed here
 IMAGE_HEIGHT = 20
@@ -14,7 +15,8 @@ IMAGE_PIXEL = IMAGE_HEIGHT * IMAGE_WIDTH
 
 CLASSES = 62
 
-
+EPOCH = 1000
+BATCH = 50
 
 
 
@@ -29,13 +31,13 @@ def main():
     # 2. convolution layer 1
     W_conv1 = weight_variable([3, 3, 1, 128], 'W_conv1')
     b_conv1 = bias_variable([128], 'b_conv1') # 这里的128个输出层中，同层的输出都共享同一个偏置吗？
-    h_conv1 = tf.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     #h_pool1 = max_pool_2x2(h_conv1)
 
     # 3. convolution layer 2
     W_conv2 = weight_variable([3, 3, 128, 128], 'W_conv2')
     b_conv2 = bias_variable([128], 'b_conv2')
-    h_conv2 = tf.relu(conv2d(h_conv1, W_conv2) + b_conv2)
+    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
 
     # 4. pool layer 1
     h_pool1 = max_pool_2x2(h_conv2)
@@ -43,12 +45,12 @@ def main():
     # 5. convolution layer 3
     W_conv3 = weight_variable([3, 3, 128, 256], 'W_conv3')
     b_conv3 = bias_variable([256],'b_conv3')
-    h_conv3 = tf.relu(conv2d(h_pool1, W_conv3) + b_conv3)
+    h_conv3 = tf.nn.relu(conv2d(h_pool1, W_conv3) + b_conv3)
 
     # 6. convolution layer 4
     W_conv4 = weight_variable([3, 3, 256, 256], 'W_conv4')
     b_conv4 = bias_variable([256], 'b_conv4')
-    h_conv4 = tf.relu(conv2d(h_conv3, W_conv4) + b_conv4)
+    h_conv4 = tf.nn.relu(conv2d(h_conv3, W_conv4) + b_conv4)
 
     # 7. pool layer 2
     h_pool2 = max_pool_2x2(h_conv4)
@@ -56,17 +58,17 @@ def main():
     # 8. convolution layer 5
     W_conv5 = weight_variable([3, 3, 256, 512], 'W_conv5')
     b_conv5 = bias_variable([512], 'b_conv5')
-    h_conv5 = tf.relu(conv2d(h_pool2, W_conv5) + b_conv5)
+    h_conv5 = tf.nn.relu(conv2d(h_pool2, W_conv5) + b_conv5)
 
     # 9. convolution layer 6
     W_conv6 = weight_variable([3, 3, 512, 512], 'W_conv6')
     b_conv6 = bias_variable([512], 'b_conv6')
-    h_conv6 = tf.relu(conv2d(h_conv5, W_conv6) + b_conv6)
+    h_conv6 = tf.nn.relu(conv2d(h_conv5, W_conv6) + b_conv6)
 
     # 10.convolution layer 7
     W_conv7 = weight_variable([3, 3, 512, 512], 'W_conv7')
     b_conv7 = bias_variable([512], 'b_conv7')
-    h_conv7 = tf.relu(conv2d(h_conv6, W_conv7) + b_conv7)
+    h_conv7 = tf.nn.relu(conv2d(h_conv6, W_conv7) + b_conv7)
 
     # 11.pool layer 3
     h_pool3 = max_pool_2x2(h_conv7)
@@ -98,6 +100,34 @@ def main():
     
     y_conv = tf.nn.softmax(tf.matmul(h_fc2_drop, W_fc3) + b_fc3)
 
+    # loss function
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices = [1]))
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    # create session
+    sess = tf.InteractiveSession()
+
+    # initialize variables
+    sess.run(tf.initialize_all_variables())
+
+    # read data
+    
+    # import street images which containt numbers
+    stnum_imgs = read_st_img('street-data/trainResized.zip')
+    stnum_labs = read_st_label('street-data/trainLabels.csv')
+    stnum_obj = simpleDataSet(stnum_imgs, stnum_labs)
+
+
+    ## train
+    for i in range(EPOCH):
+        batch = stnum_obj.next_batch(50)
+        if i % 100 == 0:
+            print 'training...', i
+        train_step.run(feed_dict={x:batch[0], y_:batch[1], keep_prob1:0.5, keep_prob2:0.5})
+
+
 # convolution
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
@@ -116,7 +146,7 @@ def weight_variable(shape, name):
 
 # bias
 def bias_variable(shape, name):
-    intial = tf.constant(0.1, shape=shape)
+    initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial, name=name)
 
 
